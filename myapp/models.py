820b5db7bb1d773bb3e58df57458e0d392e6ae27
Sakon Malila <sakon-malila@users.noreply.github.com>
2026-08-03 12:32:31 +0700

    feat: initialize Student model, admin registration, and student list view template

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib import admin
+from django.urls import reverse
 
 # Create your models here.
+#st_id, fname, lname
+
+PREFIX_NAME = (
+    ("นาย", "นาย"),
+    ("นาง", "นาง"),
+    ("นางสาว", "นางสาว"),
+)
+
+
+class Student(models.Model):
+
+    prefix = models.CharField(max_length=10, choices=PREFIX_NAME, default="นาย")
+    st_id = models.CharField(max_length=12, unique=True)
+    fname = models.CharField(max_length=100, blank=False)
+    lname = models.CharField(max_length=100, blank=False)
+
+    def __str__(self):
+        return self.prefix + " " + self.fname + " " + self.lname + " [" + self.st_id + "]"
+
+    def get_absolute_url(self):
+        return reverse('student_detail', kwargs={'pk': self.pk})
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('st_id', 'prefix', 'fname', 'lname')
+
--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,15 @@
 # pyrefly: ignore [missing-import]
 from django.shortcuts import render
 import datetime
-def index(request):
+from .models import Student
 
+def index(request):
+    student = Student.objects.all()
     context ={
        'title':'My Home Page',   
 
     }
+    context['students'] = Student.objects.all().order_by('st_id')
     context['date']= datetime.date.today()
     return render(request, "index.html",context)
 
