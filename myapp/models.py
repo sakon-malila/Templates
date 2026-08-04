a79d93a836a397996765d62028c8f3ab8d83f4eb
Sakon Malila <sakon-malila@users.noreply.github.com>
2026-08-04 12:25:26 +0700

    send homework

--- myapp/models.py
+++ myapp/models.py
@@ -1,5 +1,8 @@
+# pyrefly: ignore [missing-import]
 from django.db import models
+# pyrefly: ignore [missing-import]
 from django.contrib import admin
+# pyrefly: ignore [missing-import]
 from django.urls import reverse
 
 # Create your models here.
@@ -15,17 +18,17 @@ PREFIX_NAME = (
 class Student(models.Model):
 
     prefix = models.CharField(max_length=10, choices=PREFIX_NAME, default="นาย")
-    st_id = models.CharField(max_length=12, unique=True)
+    stu_id = models.CharField(max_length=12, unique=True)
     fname = models.CharField(max_length=100, blank=False)
     lname = models.CharField(max_length=100, blank=False)
 
     def __str__(self):
-        return self.prefix + " " + self.fname + " " + self.lname + " [" + self.st_id + "]"
+        return self.prefix + " " + self.fname + " " + self.lname + " [" + self.stu_id + "]"
 
     def get_absolute_url(self):
         return reverse('student_detail', kwargs={'pk': self.pk})
 
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('st_id', 'prefix', 'fname', 'lname')
+    list_display = ('stu_id', 'prefix', 'fname', 'lname')
 
