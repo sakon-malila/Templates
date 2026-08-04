2ed8da6170bdc12d543adc91f26c35018383b2cd
Sakon Malila <sakon-malila@users.noreply.github.com>
2026-08-04 12:16:16 +0700

    send homework

--- main/settings.py
+++ main/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / 'statics']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
